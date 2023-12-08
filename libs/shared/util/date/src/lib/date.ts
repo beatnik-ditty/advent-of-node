32b672e294c4bd955bee46dc0c2f93c9a9718994
Graham Porter <149897204+beatnik-ditty@users.noreply.github.com>
2023-12-08 15:57:54 -0600

    Added countdown timer and auto puzzle unlock to react-app (#11)

--- libs/shared/util/date/src/lib/date.ts
+++ libs/shared/util/date/src/lib/date.ts
@@ -7,7 +7,7 @@ const dateFormat = new Intl.DateTimeFormat('en-US', {
   timeZoneName: 'short',
 });
 
-export const presentDate = () => {
+export const getPresentDate = (): CalendarDay => {
   const [month, day, year] = dateFormat
     .format(new Date())
     .split(/\/|,.*$/gi)
@@ -17,19 +17,28 @@ export const presentDate = () => {
     case 12:
       return { year, day: Math.min(day, 25) };
     case 11:
-      return { year, day: 0 };
+      return { year, day: day - 30 };
     default:
       return { year: year - 1, day: 25 };
   }
 };
 
-const date: CalendarDay = presentDate();
-
-export const getPresentDate = (): CalendarDay => {
-  return { ...date };
+export const timeUntilMidnight = (): { h: number; m: number; s: number; ms: number } => {
+  // JS Date is iffy with timezones besides browser's local time and UTC, so we use UTC.
+  const now = new Date();
+  const midnight = new Date(now.getTime());
+  midnight.getUTCHours() >= 17 && midnight.setUTCDate(midnight.getUTCDate() + 1);
+  midnight.setUTCHours(5, 0, 0, 0);
+  const time = midnight.getTime() - now.getTime();
+  return {
+    h: Math.floor(time / (1000 * 60 * 60)),
+    m: Math.floor(time / (1000 * 60)) % 60,
+    s: Math.floor(time / 1000) % 60,
+    ms: time % 1000,
+  };
 };
 
 export const hasDateOccured = ({ year: testYear, day: testDay }: CalendarDay) => {
-  const { day, year } = presentDate();
+  const { day, year } = getPresentDate();
   return testYear < year || (testYear === year && testDay <= day);
 };
