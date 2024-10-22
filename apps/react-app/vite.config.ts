04aaf9ac37b9f815c3554e264b42c9b1187f5514
Graham <149897204+beatnik-ditty@users.noreply.github.com>
2024-10-22 18:29:48 -0500

    Fixed vite builds broken by Nx 20 migration.

--- apps/react-app/vite.config.ts
+++ apps/react-app/vite.config.ts
@@ -1,35 +1,42 @@
 /// <reference types='vitest' />
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => ({
-  cacheDir: '../../node_modules/.vite/react-app',
-
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/react-app',
   server: {
     port: 4200,
     host: 'localhost',
   },
-
   preview: {
     port: 4300,
     host: 'localhost',
   },
-
-  plugins: [react(), nxViteTsPaths()],
-
+  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
-
-  test: {
-    reporters: ['default'],
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
+  build: {
+    outDir: '../../dist/apps/react-app',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
+  },
+  test: {
+    watch: false,
+    globals: true,
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/react-app',
+      provider: 'v8',
+    },
   },
-}));
+});
--- libs/react/feature/header/vite.config.ts
+++ libs/react/feature/header/vite.config.ts
@@ -1,35 +1,37 @@
 /// <reference types='vitest' />
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig(() => ({
-  cacheDir: '../../../../node_modules/.vite/feature-header',
-
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../../node_modules/.vite/libs/react/feature/header',
   plugins: [
     react(),
     nxViteTsPaths(),
-    dts({
-      entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
-    }),
+    nxCopyAssetsPlugin(['*.md']),
+    dts({ entryRoot: 'src', tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json') }),
   ],
-
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
   // },
-
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: '../../../dist/libs/react/feature/header',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
-      name: 'feature-header',
+      name: 'data-access-api',
       fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
@@ -40,14 +42,15 @@ export default defineConfig(() => ({
       external: ['react', 'react-dom', 'react/jsx-runtime'],
     },
   },
-
   test: {
-    reporters: ['default'],
+    watch: false,
     globals: true,
-    cache: {
-      dir: '../../../../node_modules/.vitest',
-    },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../../coverage/libs/react/feature/header',
+      provider: 'v8',
+    },
   },
-}));
+});
