cc26f4d64f894920ca6b5bd8cfa6da042baa513d
Graham <149897204+beatnik-ditty@users.noreply.github.com>
2024-10-23 11:51:31 -0500

    Explicitly added source mapping in node apps for debugging.

--- apps/solver-app/webpack.config.js
+++ apps/solver-app/webpack.config.js
@@ -2,7 +2,7 @@ const { composePlugins, withNx } = require('@nx/webpack');
 const path = require('path');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), config => {
+module.exports = composePlugins(withNx({ sourceMap: true }), config => {
   config.output.devtoolModuleFilenameTemplate = function (info) {
     const rel = path.relative(process.cwd(), info.absoluteResourcePath);
     return `webpack:///./${rel}`;
